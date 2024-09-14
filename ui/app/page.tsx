297546770386ddf06f1eeeeafdf36806d0ad75f9
Jan Koeppen <jan@yaneq.com>
2024-09-14 18:03:37 -0300

    fetch agent results

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,45 +1,83 @@
+"use client";
 import Image from "next/image";
+import config from "../../config.json";
+import { useState } from "react";
+import { ITweet } from "./types/types";
 
 export default function Home() {
+  const [results, setResults] = useState<ITweet[]>([]);
+  const [subject, setSubject] = useState<string>("");
+  const onSubmit = () => {
+    setResults([]);
+    config.agents.forEach(async (agent) => {
+      try {
+        const startTime = performance.now();
+        const response = await fetch(`${agent.url}?subject=${subject}`);
+        const endTime = performance.now();
+        console.log(
+          `Call to ${agent.url} took ${endTime - startTime} milliseconds.`
+        );
+        const result = await response.json();
+        setResults((prevResults) => prevResults.concat(result));
+      } catch (error) {
+        console.error(`Error fetching data from ${agent.url}:`, error);
+      }
+    });
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 items-center sm:items-start">
-        {/* <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
             <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
               Social media posts for your business.
             </h2>
             <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
-              Please describe what your business is doing, and your agents will
+              Please describe what your business is doing, and our agents will
               handle the rest.
             </p>
-            <form className="mx-auto mt-10 flex max-w-md gap-x-4">
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="subject"
-                name="subject"
-                type="text"
-                required
-                placeholder="Subject"
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-              />
-              <button
-                type="submit"
-                className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Get started
-              </button>
-            </form>
+
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="subject"
+              name="subject"
+              type="text"
+              required
+              placeholder="Subject"
+              value={subject}
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+            />
+            <button
+              className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              onClick={onSubmit}
+            >
+              Get started
+            </button>
+
+            <div className="mt-10">
+              {results?.map((result, id) => (
+                <div
+                  key={id}
+                  className="tweet p-4 border-b border-gray-200 bg-white rounded mt-3"
+                >
+                  <div className="tweet-header flex items-center space-x-2">
+                    <span className="username font-bold">SocialsSwarm</span>
+                    <span className="handle text-gray-500">@socialsswarm</span>
+                    <span className="time text-gray-500">· 1h</span>
+                  </div>
+                  <div className="tweet-content mt-2 text-gray-900">
+                    {result.text}
+                  </div>
+                </div>
+              ))}
+            </div>
+
             <svg
               viewBox="0 0 1024 1024"
               aria-hidden="true"
